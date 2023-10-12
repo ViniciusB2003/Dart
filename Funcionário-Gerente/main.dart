7a69d12e5ff911c01317409718655d591f9953ff
Vinicius Barbosa <viniciusbarbosa500@outlook.com>
2023-10-11 21:02:35 -0400

    Alteracao

--- Funcionário-Gerente/main.dart
+++ Funcionário-Gerente/main.dart
@@ -1,26 +1,28 @@
 import 'dart:io';
 import 'Funcionario.dart';
-import'Gerente.dart';
+import 'Gerente.dart';
 
 void main() {
   Funcionario meuPrimeiroFuncionario = Funcionario(nomeDoInfeliz: "João");
   Gerente primeiroGerente = Gerente(nome: "Carlos");
   int tentativa = 0;
 
-  while(tentativa < 4) {
-    print("Qual Opção você prefere? 1 - Quer Aumento; 2 - Quer Reconhecimento; 3 - Quer Férias");
+  while (tentativa < 4) {
+    print(
+        "Qual Opção você prefere? 1 - Quer Aumento; 2 - Quer Reconhecimento; 3 - Quer Férias");
     String? op = stdin.readLineSync()!;
     int parsedOp = int.parse(op);
-    bool aumento = primeiroGerente.mereceaumento(meuPrimeiroFuncionario.nomeDoInfeliz);
-    
+    bool aumento =
+        primeiroGerente.mereceaumento(meuPrimeiroFuncionario.nomeDoInfeliz);
+
     if (parsedOp == 1 && aumento == false) {
       primeiroGerente.mereceaumento(meuPrimeiroFuncionario.nomeDoInfeliz!);
       tentativa += 1;
-      
+
       print("Entrou na 1");
-      
     } else if (parsedOp == 2) {
-      primeiroGerente.merecereconhecimento(meuPrimeiroFuncionario.nomeDoInfeliz!);
+      primeiroGerente
+          .merecereconhecimento(meuPrimeiroFuncionario.nomeDoInfeliz!);
       tentativa += 1;
       print("Entrou na 2");
     } else if (parsedOp == 3) {
