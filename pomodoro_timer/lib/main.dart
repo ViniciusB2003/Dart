ce741c555e2dcde7ab045e302553a47707fc5058
Vinicius Barbosa <viniciusbarbosa500@outlook.com>
2023-12-05 23:40:13 -0400

    'Adicionando Imagem'

--- pomodoro_timer/lib/main.dart
+++ pomodoro_timer/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:html';
-import 'dart:async';
 
 import 'package:flutter/material.dart';
 
@@ -44,8 +43,8 @@ class _ProgressIndicatorExampleState extends State<ProgressIndicatorExample>
     int maxRepeatCount = _counter;
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 10),
-      reverseDuration: const Duration(seconds: 15),
+      duration: const Duration(seconds: 5),
+      reverseDuration: const Duration(seconds: 5),
       value: 1,
     )
       ..addStatusListener((status) {
@@ -80,61 +79,69 @@ class _ProgressIndicatorExampleState extends State<ProgressIndicatorExample>
         backgroundColor: Color.fromRGBO(85, 24, 24, 0.965),
         title: Center(child: Text('Temporizador Pomodoro')),
       ),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            SizedBox(
-                height: 200,
-                width: 200,
-                child: Transform.scale(
-                  scale: 3,
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                        Color.fromRGBO(3, 1, 31, 0.965)),
-                    value: controller.value,
-                    semanticsLabel: 'Circular progress indicator',
-                  ),
-                )),
-            Column(
-              children: [
-                SizedBox(height: 100),
-                AnimatedBuilder(
-                  animation: controller,
-                  builder: (BuildContext context, Widget? child) {
-                    return Text(
-                      controller.status == AnimationStatus.forward
-                          ? 'Hora de Descansar'
-                          : 'Hora de Estudar',
-                      style: const TextStyle(
-                          fontSize: 40,
-                          color: Color.fromRGBO(
-                              94, 6, 6, 0.965)), // Estilo do texto
-                    );
-                  },
-                ),
-                AnimatedBuilder(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(
+                    'C:/Códigos VSCODE/Dart-Hub/pomodoro_timer/lib/assets/Estudo.gif'),
+                fit: BoxFit.cover)),
+        child: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Transform.scale(
+                    scale: 3,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                          Color.fromRGBO(3, 1, 31, 0.965)),
+                      value: controller.value,
+                      semanticsLabel: 'Circular progress indicator',
+                    ),
+                  )),
+              Column(
+                children: [
+                  SizedBox(height: 100),
+                  AnimatedBuilder(
                     animation: controller,
                     builder: (BuildContext context, Widget? child) {
-                      return Text(controller.status == AnimationStatus.completed
-                          ? 'Defina quantos ciclos serão feitos e aperte Iniciar'
-                          : '');
-                    }),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton.icon(
-                  onPressed: _incrementCounter,
-                  icon: const Icon(Icons.add),
-                  label: Text('$_counter'),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(onPressed: initState, child: Text('Iniciar')),
-              ],
-            ),
-          ],
+                      return Text(
+                        controller.status == AnimationStatus.forward
+                            ? 'Hora de Descansar'
+                            : 'Hora de Estudar',
+                        style: const TextStyle(
+                            fontSize: 40,
+                            color: Color.fromRGBO(
+                                94, 6, 6, 0.965)), // Estilo do texto
+                      );
+                    },
+                  ),
+                  AnimatedBuilder(
+                      animation: controller,
+                      builder: (BuildContext context, Widget? child) {
+                        return Text(controller.status ==
+                                AnimationStatus.completed
+                            ? 'Defina quantos ciclos serão feitos e aperte Iniciar'
+                            : '');
+                      }),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton.icon(
+                    onPressed: _incrementCounter,
+                    icon: const Icon(Icons.add),
+                    label: Text('$_counter'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(onPressed: initState, child: Text('Iniciar')),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
